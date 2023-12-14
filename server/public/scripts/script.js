c0cbbd21e97b0d59cf481f57b25756fdd49aac33
Oscar Vestlund <ove@lysator.liu.se>
2023-12-14 14:59:55 +0100

    public/script: display fetched json in log

--- server/public/scripts/script.js
+++ server/public/scripts/script.js
@@ -1,1 +1,11 @@
-console.log("hej du!");
+//console.log("hej du!");
+
+let jsondb = fetch("http://localhost:3000/users");
+
+jsondb.then((response) => {
+	//console.log(response.json());
+	return response.json();
+}).then((users) => {
+	console.log(users);
+});
+
